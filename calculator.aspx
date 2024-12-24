2a0258fea03e0ae9d200fa5f69024bd403158c46
nileshkanzariya1000 <nkanzariya582@rku.ac.in>
2024-12-24 11:43:55 +0530

    design calculater

---
+++ calculator.aspx
@@ -0,0 +1,36 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="calculator.aspx.cs" Inherits="demo.calculator" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:TextBox ID="TextBox1" runat="server" Height="81px" OnTextChanged="TextBox1_TextChanged" Width="251px"></asp:TextBox>
+            <br />
+        </div>
+        <asp:Button ID="Button1" runat="server" Height="55px" OnClick="Button1_Click" Text="7" Width="62px" />
+        <asp:Button ID="Button2" runat="server" Height="55px" OnClick="Button2_Click" Text="8" Width="62px" />
+        <asp:Button ID="Button3" runat="server" Height="55px" OnClick="Button3_Click" Text="9" Width="62px" />
+        <asp:Button ID="Button4" runat="server" Height="55px" OnClick="Button4_Click" Text="/" Width="62px" />
+        <br />
+        <asp:Button ID="Button5" runat="server" Height="55px" OnClick="Button5_Click" Text="4" Width="62px" />
+        <asp:Button ID="Button6" runat="server" Height="55px" OnClick="Button6_Click" Text="5" Width="62px" />
+        <asp:Button ID="Button7" runat="server" Height="55px" OnClick="Button7_Click" Text="6" Width="62px" />
+        <asp:Button ID="Button8" runat="server" Height="55px" Text="-" Width="62px" />
+        <br />
+        <asp:Button ID="Button9" runat="server" Height="55px" OnClick="Button9_Click" Text="1" Width="62px" />
+        <asp:Button ID="Button10" runat="server" Height="55px" OnClick="Button10_Click" Text="2" Width="62px" />
+        <asp:Button ID="Button11" runat="server" Height="55px" OnClick="Button11_Click" Text="3" Width="62px" />
+        <asp:Button ID="Button12" runat="server" Height="55px" OnClick="Button12_Click" Text="+" Width="62px" />
+        <br />
+        <asp:Button ID="Button13" runat="server" Height="55px" OnClick="Button13_Click" Text="0" Width="62px" />
+        <asp:Button ID="Button14" runat="server" Height="55px" OnClick="Button14_Click" Text="=" Width="62px" />
+        <asp:Button ID="Button15" runat="server" Height="55px" OnClick="Button15_Click" Text="clear" Width="62px" />
+        <asp:Button ID="Button16" runat="server" Height="55px" Text="*" Width="62px" />
+    </form>
+</body>
+</html>
---
+++ calculator.aspx.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace demo
+{
+    public partial class calculator : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        double x = 0;
+        double y = 0;
+        string op = "";
+
+        protected void Button13_Click(object sender, EventArgs e)
+        {
+            TextBox1.Text += "0";
+        }
+
+        protected void Button9_Click(object sender, EventArgs e)
+        {
+            TextBox1.Text += "1";
+
+        }
+
+        protected void Button10_Click(object sender, EventArgs e)
+        {
+            TextBox1.Text += "2";
+
+        }
+
+        protected void Button11_Click(object sender, EventArgs e)
+        {
+            TextBox1.Text += "3";
+
+        }
+
+        protected void Button5_Click(object sender, EventArgs e)
+        {
+            TextBox1.Text += "4";
+
+        }
+
+        protected void Button6_Click(object sender, EventArgs e)
+        {
+            TextBox1.Text += "5";
+
+        }
+
+        protected void Button7_Click(object sender, EventArgs e)
+        {
+            TextBox1.Text += "6";
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            TextBox1.Text += "7";
+
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            TextBox1.Text += "8";
+
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            TextBox1.Text += "9";
+
+        }
+
+        protected void Button15_Click(object sender, EventArgs e)
+        {
+            TextBox1.Text = "";
+        }
+
+        protected void Button12_Click(object sender, EventArgs e)
+        {
+            x=Convert.ToDouble(TextBox1.Text);
+            TextBox1.Text = "";
+            op = "+";
+            Response.Write(x);
+        }
+
+        protected void Button14_Click(object sender, EventArgs e)
+        {
+            y=Convert.ToDouble(TextBox1.Text);
+            double sum = x + y;
+                Response.Write(sum);
+            
+
+        }
+    }
+}
